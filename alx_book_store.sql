7c7475b7f0643e13905ab44b89d487a507f412b9
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-05 00:39:31 +0300

    task0

---
+++ alx_book_store.sql
@@ -0,0 +1,51 @@
+CREATE TABLE Books(
+book_id INT PRIMARY KEY, 
+title VARCHAR(130),
+author_id INT, 
+price DOUBLE, 
+publication_date DATE);
+
+CREATE TABLE Authors(
+author_id INT PRIMARY KEY, 
+author_name VARCHAR(215));
+
+CREATE TABLE Customers(
+customer_id INT PRIMARY KEY, 
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT);
+
+CREATE TABLE Orders(
+order_id INT PRIMARY KEY, 
+customer_id INT, 
+order_date DATE);
+
+CREATE TABLE Order_Details(
+orderdetailid INT PRIMARY KEY, 
+order_id INT, 
+book_id INT, 
+quantity DOUBLE);
+
+ALTER TABLE Books 
+ADD CONSTRAINT author_id
+FOREIGN KEY (author_id)
+REFERENCES Authors (author_id);
+
+ALTER TABLE Orders
+ADD CONSTRAINT customer_id
+FOREIGN KEY (customer_id)
+REFERENCES Customers (customer_id);
+
+ALTER TABLE Order_Details
+ADD CONSTRAINT order_id
+FOREIGN KEY (order_id)
+REFERENCES Orders (order_id);
+
+ALTER TABLE Order_Details
+ADD CONSTRAINT book_id
+FOREIGN KEY (book_id)
+REFERENCES Books (book_id);
+
+
+
+
