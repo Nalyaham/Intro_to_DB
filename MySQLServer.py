e07c7eb4e9e95677c3e896ac2bec74825b62e51a
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-04 16:17:24 +0300

    task0

---
+++ MySQLServer.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+# Replace with your connection details
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Levites123",
+    database="alx_book_store"
+)
+
+print(mydb.get_server_info())
