2de919d88da5bd7d894403eea6040734e27f24c3
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-05 03:08:25 +0300

    task1

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,17 +1,24 @@
 import mysql.connector
 
+try:
+    
 # Database connection details (replace with your own)
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Levites123"
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Levites123"
 )
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
 # Create a table named `customers` (if it doesn't exist)
-mycursor.execute("""
-CREATE DATABASE IF NOT EXISTS alx_book_store;""")
+    mycursor.execute("""CREATE DATABASE IF NOT EXISTS alx_book_store;""")
+ 
+except mysql.connector.Error as err:
+    if err.errno == 1007:  # Database already exists error code
+        print(f"Database '{database_name}' already exists. No need to create.")
+    else:
+        print(f"Error creating database: {err}")
 
 mycursor.close()
 mydb.close()
