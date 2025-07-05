ad64984410e8159a76f107ac5bb7fb2bb28e98f2
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-05 03:02:36 +0300

    task1

---
+++ MySQLServer.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+# Database connection details (replace with your own)
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Levites123"
+)
+
+mycursor = mydb.cursor()
+
+# Create a table named `customers` (if it doesn't exist)
+mycursor.execute("""
+CREATE DATABASE alx_book_store;""")
+
+mycursor.close()
+mydb.close()
